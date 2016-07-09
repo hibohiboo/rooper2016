8eafae1ad0630f9b71d9809d5761cfcf240b22bf
MasterScreenBot <masterscreen.bot@gmail.com>
2016-07-09 17:01:27 +0900

    cssのファイル名が間違っていたので修正

--- share/angular2/style.css
+++
@@ -1,6 +0,0 @@
-// test
-(function(app) {
-  document.addEventListener('DOMContentLoaded', function() {
-    ng.platformBrowserDynamic.bootstrap(app.AppComponent);
-  });
-})(window.app || (window.app = {}));
--- share/gulp/tasks/task.js
+++ share/gulp/tasks/task.js
@@ -1,16 +1,23 @@
 var gulp = require('/home/vagrant/gulp/node_modules/gulp');
 var spawn = require('child_process').spawn;
 
-gulp.task('default', ['copy', 'watch']);
+var angular2_dir = "/home/vagrant/angular2-quickstart";
+
+gulp.task('default', ['copy', 'watch', 'angular2']);
 
 gulp.task('copy',function(){
     var base_dir = "/home/vagrant/share/angular2",
-        dist_dir = "/home/vagrant/angular2-quickstart";
+        dist_dir = angular2_dir;
     return gulp.src(
         [ base_dir + '/*.*', base_dir + '/app/*' ],
         { "base": base_dir }
     )
     .pipe( gulp.dest( dist_dir ) );
+});
+
+gulp.task('angular2', function(){
+  spawn('npm',['start'], {cwd:angular2_dir});
+  //, {stdio: ['pipe', process.stdout, process.stderr]});
 });
 
 gulp.task('watch',['copy'],function(){
