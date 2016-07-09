75b6e84c0f8df017b2323e7755ec3ce97af2197c
MasterScreenBot <masterscreen.bot@gmail.com>
2016-07-09 16:13:05 +0900

    gulpでdocker内のフォルダにshareフォルダで編集した内容をコピー

--- Docker/shell/angular.sh
+++ Docker/shell/angular.sh
@@ -4,8 +4,8 @@ source "/vagrant/Docker/shell/env.conf"
 
 imageName="hibo/angular2"
 containerName="angular2"
-host_src_dir="/home/vagrant/angular2/app"
-container_src_dir="/home/vagrant/angular2-quickstart/app"
+host_src_dir="/vagrant/share"
+container_src_dir="/home/vagrant/share"
 docker_dir="${path_Dockerfile_dir}/angular2"
 
 # -t: docker側の標準出力をホストの標準出力につなげる。
@@ -14,20 +14,24 @@ docker_dir="${path_Dockerfile_dir}/angular2"
 # -e:環境変数
 # -u:user
 # -v: ディレクトリ共有
+# -w: ワークディレクトリ
 run(){
   docker run --name $containerName --rm -it \
              -p 80:3000 \
-             -m "300M" --memory-swap "1G" \
+             -m "300M" --memory-swap "512M" \
              -e "NODE_ENV=production" \
-             -u vagrant \
              -v $host_src_dir:$container_src_dir \
              $imageName 
 }
 
 run_d(){
-  docker run --name $containerName -i -d -p 80:3000 -v $host_src_dir:$container_src_dir $imageName
+  docker run --name $containerName -d -i \
+             -p 80:3000 \
+             -m "300M" --memory-swap "512M" \
+             -e "NODE_ENV=production" \
+             -v $host_src_dir:$container_src_dir \
+             $imageName 
 }
-
 
 checkBeforeContainer $containerName
 default $#
