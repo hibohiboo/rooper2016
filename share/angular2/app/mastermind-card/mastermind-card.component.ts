43680a7df956b90a0a5ab36e9669d9fe4b956f6c
MasterScreenBot <masterscreen.bot@gmail.com>
2016-07-30 21:03:06 +0900

    キャラクターリスト外だし

--- share/angular2/app/mastermind-card/mastermind-card.component.ts
+++ share/angular2/app/mastermind-card/mastermind-card.component.ts
@@ -1,15 +1,19 @@
 
 import { Component, Input } from '@angular/core';
-
+import { Character } from '../models/character';
 @Component({
   selector: 'mastermind-card',
   template: `
   <h2>非公開シート</h2>
   <ul>
-  <li>test</li>
+    <li>test</li>
+    <li *ngFor="let character of characters">
+        <span class="badge">{{character.id}}</span> {{character.name}}
+    </li>
   </ul>
   `
 })
 export class MastermindCardComponent {
-
+  @Input()
+  characters: Character[];
 }
--- share/angular2/app/scenario/character-list.component.ts
+++ share/angular2/app/scenario/character-list.component.ts
@@ -1,24 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {Character} from '../models/character';
 import { CharacterService } from '../services/character.service';
+import { MastermindCardComponent } from '../mastermind-card';
 // import '../../assets/css/styles.css';
 @Component({
   selector: 'character-list',
   templateUrl: './character-list.component.html',
   styleUrls: ['./character-list.component.css'],
   providers: [CharacterService]
+  ,directives: [MastermindCardComponent]
 })
 export class CharacterListComponent {
   title = '脚本作成';
   characters: Character[];
   selectedCharacter: Character;
-  constructor(private characterService: CharacterService) { }
+  selectedCharacters: Character[];
+  constructor(private characterService: CharacterService) {  }
   getCharacters() {
     this.characterService.getCharacters().then(characters => this.characters = characters);
   }
   ngOnInit() {
     this.getCharacters();
+    this.selectedCharacters = [];
   }
-  onSelect(character: Character) { this.selectedCharacter = character; }
+  onSelect(character: Character) {
+       this.selectedCharacter = character;
+       this.selectedCharacters.push(character);
+  }
 
  }
